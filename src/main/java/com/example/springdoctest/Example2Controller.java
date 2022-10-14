380fa9993dde1aa25e7e6aa3dbc4fdb3599fb761
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2022-10-15 00:26:29 +0900

    exceptionhandler確認

---
+++ src/main/java/com/example/springdoctest/Example2Controller.java
@@ -0,0 +1,29 @@
+package com.example.springdoctest;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/example2")
+public class Example2Controller {
+  @ExceptionHandler(Exception.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  @ApiResponse(responseCode = "500", description = "ExceptionHandler in example2")
+  public ErrorResponse customControllerException() {
+    return new ErrorResponse("1", "example2");
+  }
+
+  public record ErrorResponse(String errorId, String message) {}
+
+  @GetMapping("/500")
+  @Operation(
+      tags = "example2",
+      summary = "Example2 method",
+      description = "This method is an example2"
+  )
+  public void test() {
+    throw new RuntimeException();
+  }
+}
---
+++ src/main/java/com/example/springdoctest/ExampleController.java
@@ -0,0 +1,27 @@
+package com.example.springdoctest;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/example")
+public class ExampleController {
+  @ExceptionHandler(Exception.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  @ApiResponse(responseCode = "500", description = "ExceptionHandler in example")
+  public String customControllerException() {
+    return "example";
+  }
+
+  @GetMapping("/500")
+  @Operation(
+      tags = {"example"},
+      summary = "Example method",
+      description = "This method is an example"
+  )
+  public void test() {
+    throw new RuntimeException();
+  }
+}
